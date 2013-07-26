671a9d204ae50aafdcbc7405b2d673ee520ccfe5
draggi <dragan@rfq-hub.com>
2013-07-26 17:44:18 +0200

    First version
    cool!

---
+++ TreadmillController/TreadmillControllerForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TreadmillController
+{
+    partial class TreadmillControllerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MphInfo = new System.Windows.Forms.Label();
+            this.KmInfo = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.Stop = new System.Windows.Forms.Button();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Purple;
+            this.label2.Location = new System.Drawing.Point(312, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "miles/h";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(168, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 18);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "km/h";
+            // 
+            // MphInfo
+            // 
+            this.MphInfo.AutoSize = true;
+            this.MphInfo.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MphInfo.ForeColor = System.Drawing.Color.Purple;
+            this.MphInfo.Location = new System.Drawing.Point(245, 39);
+            this.MphInfo.Name = "MphInfo";
+            this.MphInfo.Size = new System.Drawing.Size(35, 38);
+            this.MphInfo.TabIndex = 15;
+            this.MphInfo.Text = "0";
+            // 
+            // KmInfo
+            // 
+            this.KmInfo.AutoSize = true;
+            this.KmInfo.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KmInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.KmInfo.Location = new System.Drawing.Point(87, 40);
+            this.KmInfo.Name = "KmInfo";
+            this.KmInfo.Size = new System.Drawing.Size(35, 38);
+            this.KmInfo.TabIndex = 14;
+            this.KmInfo.Text = "0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Menu;
+            this.trackBar1.Location = new System.Drawing.Point(23, 12);
+            this.trackBar1.MinimumSize = new System.Drawing.Size(45, 187);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 304);
+            this.trackBar1.TabIndex = 13;
+            // 
+            // Stop
+            // 
+            this.Stop.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop.Location = new System.Drawing.Point(46, 341);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(119, 59);
+            this.Stop.TabIndex = 12;
+            this.Stop.Text = "STOP";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(404, 422);
+            this.shapeContainer1.TabIndex = 18;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 67;
+            this.lineShape1.X2 = 375;
+            this.lineShape1.Y1 = 87;
+            this.lineShape1.Y2 = 87;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 217;
+            this.lineShape2.X2 = 217;
+            this.lineShape2.Y1 = 23;
+            this.lineShape2.Y2 = 87;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(279, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "label3";
+            // 
+            // TreadmillControllerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 422);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MphInfo);
+            this.Controls.Add(this.KmInfo);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "TreadmillControllerForm";
+            this.Text = "Treadmill Controller";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MphInfo;
+        private System.Windows.Forms.Label KmInfo;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button Stop;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TreadmillController/TreadmillControllerForm.cs
@@ -0,0 +1,199 @@
+﻿using LockMonitor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreadmillController
+{
+    public partial class TreadmillControllerForm : Form
+    {
+         //KeyboardHook hook = new KeyboardHook();
+
+          SerialPort port = new SerialPort("COM5", 115200, Parity.None, 8, StopBits.One);
+          string SpeedToRecover = "";
+          // from wtsapi32.h
+          private const int NotifyForThisSession = 0;
+
+          // from winuser.h
+          private const int SessionChangeMessage = 0x02B1;
+          private const int SessionLockParam = 0x7;
+          private const int SessionUnlockParam = 0x8;
+
+          [DllImport("wtsapi32.dll")]
+          private static extern bool WTSRegisterSessionNotification(IntPtr hWnd, int dwFlags);
+
+          [DllImport("wtsapi32.dll")]
+          private static extern bool WTSUnRegisterSessionNotification(IntPtr hWnd);
+
+          // flag to indicate if we've registered for notifications or not
+          private bool registered = false;
+
+          /// <summary>
+          /// Is this form receiving lock / unlock notifications
+          /// </summary>
+          protected bool ReceivingLockNotifications
+          {
+              get { return registered; }
+          }
+
+
+          public TreadmillControllerForm()
+        {
+            InitializeComponent();
+              // For detecting hotkey
+
+            // Set up the TrackBar.            
+            trackBar1.Scroll += new System.EventHandler(trackBar1_Scroll);
+            trackBar1.Minimum = 8;
+            trackBar1.Maximum = 30;
+            trackBar1.TickFrequency = 2;
+            trackBar1.LargeChange = 3;
+            trackBar1.SmallChange = 1;
+            
+        }
+
+          private void TreadmillControllerForm_Load(object sender, EventArgs e)
+        {
+            MphInfo.Text = "0";
+            KmInfo.Text = "0";
+            port.DtrEnable = false;
+            port.ReadBufferSize = 100;
+        }
+
+        private void Form1_Closing(object sender, System.EventArgs e)
+        {      
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // Display the trackbar value in the text box.
+            //label3.Text = "" + (trackBar1.Value.ToString());
+            SendSpeedToTreadmill((trackBar1.Value*0.2).ToString());
+        }
+        private string ReturnRatioForSelectedMphSpeed(string mph)
+        {
+            string RatioForThisSpeed="";
+            double calc = (-0.0620747 * Convert.ToDouble(mph) + 0.92);
+            RatioForThisSpeed = calc.ToString();
+            RatioForThisSpeed = RatioForThisSpeed.Replace(",", ".");                       
+            //label3.Text = RatioForThisSpeed;
+            if (calc >= 0.82) RatioForThisSpeed = "1";
+            return RatioForThisSpeed;
+        }
+
+        private void SendSpeedToTreadmill(string value)
+        {                        
+            port.Open();
+            string read;
+            port.Write("\n");
+            //label3.Text=ReturnRatioForSelectedMphSpeed(value).ToString();
+            port.Write(ReturnRatioForSelectedMphSpeed(value));
+            port.Write("\n");
+            read = port.ReadLine();
+            MphInfo.Text = ReturnTrueSpeed(value,"mph");
+            KmInfo.Text = ReturnTrueSpeed(value, "km");
+            //label3.Text = read;
+            port.Close();
+        }
+
+        private void SendStopToTreadMill()
+        {
+            port.Open();
+            port.Write("\n");
+            port.Write("1");
+            trackBar1.Value = 8;
+            MphInfo.Text = "0";
+            KmInfo.Text = "0";
+            port.Write("\n");
+            port.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        public string ReturnTrueSpeed(string value, string type)
+        {
+            string ret = "";
+            double c=Convert.ToDouble(value);
+            if (type == "mph" & (Convert.ToDouble(value)>1.6))
+            {
+                ret = (c - 1).ToString();
+            }
+            else if (Convert.ToDouble(value) > 1.6 )
+            {
+                ret = ((c - 1) * 1.6).ToString();
+            }
+            else
+            {
+                ret = "0";
+            }
+            return ret;
+        }
+        
+        private void Stop_Click(object sender, EventArgs e)
+        {            
+            SendStopToTreadMill();
+        }
+
+        protected virtual void OnSessionLock()
+        {
+            //AddLog("Session locked");
+            SpeedToRecover = trackBar1.Value.ToString();
+            SendStopToTreadMill();
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            // check for session change notifications
+            if (m.Msg == SessionChangeMessage)
+            {
+                if (m.WParam.ToInt32() == SessionLockParam)
+                    OnSessionLock();
+                else if (m.WParam.ToInt32() == SessionUnlockParam)
+                    OnSessionUnlock();
+            }
+
+            base.WndProc(ref m);
+            return;
+        }
+
+        /// <summary>
+        /// The windows session has been unlocked
+        /// </summary>
+        protected virtual void OnSessionUnlock()
+        {
+            trackBar1.Value=Convert.ToInt16(SpeedToRecover);
+            SendSpeedToTreadmill((Convert.ToInt16(SpeedToRecover) * 0.2).ToString());        
+        }
+
+        /// <summary>
+        /// Register for event notifications
+        /// </summary>
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            // WtsRegisterSessionNotification requires Windows XP or higher
+            bool haveXp = Environment.OSVersion.Platform == PlatformID.Win32NT &&
+                                (Environment.OSVersion.Version.Major > 5 ||
+                                    (Environment.OSVersion.Version.Major == 5 &&
+                                     Environment.OSVersion.Version.Minor >= 1));
+            if (haveXp)
+                registered = WTSRegisterSessionNotification(Handle, NotifyForThisSession);            
+            return;
+        }
+
+
+    }
+}
